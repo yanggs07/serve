8882a81ee40e6190f30e692bdbffeba5ec763286
Yang Guoshuai <yangguoshuai@ethercap.com>
2020-06-16 09:35:52 +0800

    add composer

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo 111;
+
+require 'vendor/autoload.php';
+
+$url = '';
+if (isset($_SERVER['REQUEST_URI'])) {
+    parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $query);
+    if (isset($query['q'])) {
+        $url = $query['q'];
+    }
+}
+if (!$url) {
+    $url = 'test.jpg';
+}
+echo $url;
